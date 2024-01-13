403f3c5bfeffa2014f57fb863e9a6c0bbaa5dfbf
loosand <loosand@163.com>
2024-01-13 14:33:15 +0800

    build: 配置Axios、zustand以及目录、环境变量、路径别名

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { join } from 'path'
+import bundleAnalyzer from 'rollup-plugin-bundle-analyzer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), bundleAnalyzer({ analyzerMode: 'static' })],
+  resolve: {
+    alias: {
+      '@': join(__dirname, './src/'),
+    },
+  },
 })
