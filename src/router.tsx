6caac3aefdbbdf42cddd7079c60f1e9489203e7a
loosand <loosand@163.com>
2024-01-14 16:31:31 +0800

    build: 切换到对象式路由

--- src/router.tsx
+++ src/router.tsx
@@ -1,15 +1,36 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { createBrowserRouter } from 'react-router-dom'
+import Layout from './layout/Dashboard'
+import Home, { loader as homeLoader, action as homeAction } from '@/pages/Home'
+import ErrorPage from '@/pages/ErrorPage'
+import Login from './pages/Login'
+import Register from './pages/Register'
 
-export default function RouterPage() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='*' element={<PageNotFound />} />
-      </Routes>
-    </BrowserRouter>
-  )
-}
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        // index: true,
+        path: '/home/:id',
+        element: <Home />,
+        loader: ({ params }) => homeLoader(params.id),
+        action: homeAction,
+      },
+    ],
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+  {
+    path: '/register',
+    element: <Register />,
+  },
+  {
+    path: '*',
+    element: <ErrorPage />,
+  },
+])
 
-function PageNotFound() {
-  return <div>404</div>
-}
+export default router
